afc32df0353c7b52cdbebac6f7ceddfb3afd8fe2
Chidera <42821245+chideraike@users.noreply.github.com>
2020-03-11 11:42:21 +0000

    Add files via upload

---
+++ bubble-sort/10640061_bubbleSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+//10640061
+using namespace std;
+int main ()
+{
+   int i, j, temp;
+   int size;
+    // Taking Size of the Array
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+	int a[size];
+	// Taking Input in Array
+	for (int y=0; y<size; y++){
+        cout << "Enter " << y << " element: ";
+        cin >> a[y];
+	}
+   cout <<"Input list ...\n";
+   for(i = 0; i<size; i++) {
+      cout <<a[i]<<"\t";
+   }
+    cout<<endl;
+    for(i = 0; i<size; i++) {
+        for(j = i+1; j<size; j++)
+        {
+            if(a[j] < a[i]){
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        };
+    }
+    cout <<"Sorted Element List ...\n";
+    for(i = 0; i<size; i++) {
+        cout <<a[i]<<"\t";
+    }
+    return 0;
+}
---
+++ selection-sort/10640061_selectionSort.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+//10640061
+
+using namespace std;
+int findSmallest (int[],int);
+int main ()
+{
+    int size = 5;
+	int myarray[size];
+	// Taking Input in Array
+	for (int y=0; y<size; y++){
+        cout << "Enter " << y << " element: ";
+        cin >> myarray[y];
+	}
+    int pos, temp;
+    cout<<"\nInput list of elements to be Sorted\n";
+    for(int i=0;i<size;i++)
+    {
+        cout<<myarray[i]<<"\t";
+    }
+    for(int i=0;i<size;i++)
+    {
+        pos = findSmallest (myarray,i);
+        temp = myarray[i];
+        myarray[i]=myarray[pos];
+        myarray[pos] = temp;
+    }
+    cout<<"\nSorted list of elements is\n";
+    for(int i=0;i<size;i++)
+    {
+        cout<<myarray[i]<<"\t";
+    }
+
+    return 0;
+}
+int findSmallest(int myarray[],int i)
+{
+    int size = 5;
+    int ele_small,position,j;
+    ele_small = myarray[i];
+    position = i;
+    for(j=i+1;j<size;j++)
+    {
+        if(myarray[j]<ele_small)
+        {
+            ele_small = myarray[j];
+            position=j;
+        }
+    }
+    return position;
+}
+
